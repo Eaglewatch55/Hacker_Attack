0362052b851b684bd464767a5794113c78cf6f17
Eaglewatch55 <orlandogarcia94@hotmail.com>
2023-06-20 13:30:58 -0600

    Stage 2

---
+++ queries.sql
@@ -0,0 +1,71 @@
+-- STAGE 1
+CREATE TABLE "person"(
+  "person_id" VARCHAR(9) PRIMARY KEY,
+  "full_name" TEXT,
+  "address" TEXT,
+  "building_number" TEXT,
+  "phone_number" TEXT
+);
+
+.mode csv
+.import --skip 1 person.csv person
+.mode column
+
+SELECT 
+    person_id, 
+    full_name
+FROM
+    person
+ORDER BY
+    person_id
+LIMIT
+    5;
+
+-- STAGE 2
+
+CREATE TABLE "teacher"(
+    "person_id" VARCHAR(9) PRIMARY KEY,
+    "class_code" TEXT
+);
+
+.mode csv
+.import --skip 1 teacher.csv teacher
+.mode column
+
+SELECT 
+    person_id,
+    full_name
+FROM 
+    person
+WHERE person_id IN (
+    SELECT 
+        person_id
+    FROM 
+        person
+    EXCEPT
+    SELECT
+        person_id
+    FROM
+        teacher
+)
+ORDER BY
+    full_name
+LIMIT
+    5;
+
+SELECT 
+    COUNT(person_id) 
+FROM
+    person
+WHERE person_id IN (
+    SELECT 
+        person_id
+    FROM 
+        person
+    EXCEPT
+    SELECT
+        person_id
+    FROM
+        teacher
+);
+
